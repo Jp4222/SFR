3e4d45190610fbc94aca95e9de966c503764c466
Jp4222 <140108898+Jp4222@users.noreply.github.com>
2024-06-12 16:27:35 -0500

    Carritoterminado

--- Carrito/carrito.php
+++ Carrito/carrito.php
@@ -1,66 +1,142 @@
-<?php 
-$menesaje="";
-if(isset($_POST['btnAccion'])){
+<?php
+session_start();
+include 'global/config1.php';
+include 'carrito.php';
+include 'templates/cabecera.php';
 
-switch ($_POST['btnAccion']){
-    
-    case 'Agregar':
-        if(is_numeric(openssl_decrypt($_POST['id'],COD,KEY))){
-            $ID=openssl_decrypt($_POST['id'],COD,KEY);
-            $menesaje='okey ID correcto'.$ID;
-        }else{
-            $menesaje='ups ID incorrecto'.$ID;
-        }
-        if(is_string(openssl_decrypt($_POST['nombre'],COD,KEY))){
-            $NOMBRE=openssl_decrypt($_POST['nombre'],COD,KEY);
-            $menesaje.="ok Nombre".$NOMBRE."<br/>";
-            }else{$menesaje="upps.. algo pasa con el nombre"; break;}
-        if(is_string(openssl_decrypt($_POST['cantidad'],COD,KEY))){
-            $CANTIDAD=openssl_decrypt($_POST['cantidad'],COD,KEY);
-            $menesaje.="ok Cantidad".$CANTIDAD."<br/>";
-            }else{$menesaje="upps.. algo pasa con el cantidad"; break;}
-        if(is_string(openssl_decrypt($_POST['precio'],COD,KEY))){
-            $PRECIO=openssl_decrypt($_POST['precio'],COD,KEY);
-            $menesaje.="ok Precio".$PRECIO."<br/>";
-            }else{$menesaje="upps.. algo pasa con el precio"; break;}
-            if (!isset($_SESSION['CARRITO2'])){
-                $producto= array (
-                    'ID'=>$ID,
-                    'NOMBRE'=>$NOMBRE,
-                    'CANTIDAD'=>$CANTIDAD,
-                    'PRECIO'=>$PRECIO,  
-                );
-                $_SESSION['CARRITO2'][0]=$producto;
-            }else{
-                $NumeroProductos=count($_SESSION['CARRITO2']);
-                $producto= array (
-                    'ID'=>$ID,
-                    'NOMBRE'=>$NOMBRE,
-                    'CANTIDAD'=>$CANTIDAD,
-                    'PRECIO'=>$PRECIO,  
-                );
-                $_SESSION['CARRITO2'][$NumeroProductos]=$producto;
+//var_dump($_SESSION['CARRITO1']);//
+?>
+
+<br>
+<h3>Lista Del Carrito</h3>
+<?php if(!empty($_SESSION['CARRITO2'])) { ?>
+<table class="table table-light table-bordered">
+    <tbody>
+        <tr>
+            <th width="40%" class="text-center">Nombre</th>
+            <th width="15%" class="text-center">Cantidad</th>
+            <th width="20%" class="text-center">Precio</th>
+            <th width="20%" class="text-center">Total</th>
+            <th width="5%">--</th>
+        </tr>
+        <?php $total = 0; ?>
+        <?php foreach($_SESSION['CARRITO2'] as $indice => $producto) { ?>
+        <tr>
+            <td width="40%" class="text-center"><?php echo $producto['NOMBRE'] ?></td>
+            <td width="15%" class="text-center"><?php echo $producto['CANTIDAD'] ?></td>
+            <td width="20%" class="text-center"><?php echo $producto['PRECIO'] ?></td>
+            <td width="20%" class="text-center"><?php echo number_format($producto['PRECIO'] * $producto['CANTIDAD'], 2); ?></td>
+            <td width="5%">
+                <form action="" method="post">
+                    <input type="hidden" name="id" id="id" value="<?php echo openssl_encrypt($producto['ID'], COD, KEY); ?>">
+                    <button class="btn btn-danger" type="submit" name="btnAccion" value="Eliminar">Eliminar</button>
+                </form>
+            </td>
+        </tr>
+        <?php $total += ($producto['PRECIO'] * $producto['CANTIDAD']); ?>
+        <?php } ?>
+        <tr>
+            <td colspan="3" align="right"><h3>Total</h3></td>
+            <td align="right"><h3><?php echo number_format($total, 2); ?></h3></td>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
+<?php } else { ?>
+    <div class="alert alert-success" role="alert">
+        No hay productos en el carrito...
+    </div>
+<?php } ?>
+
+<?php
+if (isset($_POST['btnAccion'])) {
+
+    switch ($_POST['btnAccion']) {
+
+        case 'Agregar':
+            if (is_numeric(openssl_decrypt($_POST['id'], COD, KEY))) {
+                $ID = openssl_decrypt($_POST['id'], COD, KEY);
+                $mensaje = 'okey ID correcto ' . $ID;
+            } else {
+                $mensaje = 'ups ID incorrecto';
+                break;
             }
-            $menesaje=print_r($_SESSION,true);
-        break;
-        case "Eliminar";
+            if (is_string(openssl_decrypt($_POST['nombre'], COD, KEY))) {
+                $NOMBRE = openssl_decrypt($_POST['nombre'], COD, KEY);
+                $mensaje .= "ok Nombre " . $NOMBRE . "<br/>";
+            } else {
+                $mensaje = "upps.. algo pasa con el nombre";
+                break;
+            }
+            if (is_numeric(openssl_decrypt($_POST['cantidad'], COD, KEY))) {
+                $CANTIDAD = openssl_decrypt($_POST['cantidad'], COD, KEY);
+                $mensaje .= "ok Cantidad " . $CANTIDAD . "<br/>";
+            } else {
+                $mensaje = "upps.. algo pasa con la cantidad";
+                break;
+            }
+            if (is_numeric(openssl_decrypt($_POST['precio'], COD, KEY))) {
+                $PRECIO = openssl_decrypt($_POST['precio'], COD, KEY);
+                $mensaje .= "ok Precio " . $PRECIO . "<br/>";
+            } else {
+                $mensaje = "upps.. algo pasa con el precio";
+                break;
+            }
 
-            if(is_numeric( openssl_decrypt($_POST['id'],COD,KEY))){
-                $ID=openssl_decrypt($_POST['id'],COD,KEY);
-              
-                foreach($_SESSION['CARRITO2'] as $indice=>$producto){
-                    if($producto['ID']==$ID){
+            // Verificar si el producto ya existe en el carrito
+            if (!isset($_SESSION['CARRITO2'])) {
+                $_SESSION['CARRITO2'] = array();
+            }
 
-                        unset($_SESSION['CARRITO2'][$indice]);
-                        echo "<script>alert('Elemento borrado...');</script>";
-
-                    }
-
-
+            $productoEncontrado = false;
+            foreach ($_SESSION['CARRITO2'] as $indice => $producto) {
+                if ($producto['ID'] == $ID) {
+                    // Si el producto ya existe, actualizar la cantidad
+                    $_SESSION['CARRITO2'][$indice]['CANTIDAD'] += $CANTIDAD;
+                    $productoEncontrado = true;
+                    break;
                 }
-            }else{
-                $menesaje='ups ID incorrecto'.$ID."<br/>";
             }
-        break;
+
+            if (!$productoEncontrado) {
+                // Si el producto no existe, agregarlo al carrito
+                $producto = array(
+                    'ID' => $ID,
+                    'NOMBRE' => $NOMBRE,
+                    'CANTIDAD' => $CANTIDAD,
+                    'PRECIO' => $PRECIO,
+                );
+                $_SESSION['CARRITO2'][] = $producto;
+            }
+
+            $mensaje = print_r($_SESSION, true);
+            break;
+
+        case "Eliminar":
+            if (is_numeric(openssl_decrypt($_POST['id'], COD, KEY))) {
+                $ID = openssl_decrypt($_POST['id'], COD, KEY);
+
+                foreach ($_SESSION['CARRITO2'] as $indice => $producto) {
+                    if ($producto['ID'] == $ID) {
+                        if ($_SESSION['CARRITO2'][$indice]['CANTIDAD'] > 1) {
+                            // Reducir la cantidad en uno
+                            $_SESSION['CARRITO2'][$indice]['CANTIDAD'] -= 1;
+                        } else {
+                            // Eliminar el producto del carrito
+                            unset($_SESSION['CARRITO2'][$indice]);
+                        }
+
+                        // Reindexar el array para evitar problemas de índices no consecutivos
+                        $_SESSION['CARRITO2'] = array_values($_SESSION['CARRITO2']);
+
+                        echo "<script>alert('Cantidad actualizada en el carrito.');</script>";
+                        break;
+                    }
+                }
+            } else {
+                $mensaje = 'ups ID incorrecto' . "<br/>";
+            }
+            break;
+    }
 }
-}
+?>
--- Carrito/mostrarCarrito.php
+++ Carrito/mostrarCarrito.php
@@ -13,53 +13,84 @@ include 'templates/cabecera.php';
 <table class="table table-light table-bordered">
     <tbody>
         <tr>
-            <th width="40%" class="text-center">Descripcion</th>
-            <th width="15%" class="text-center">cantidad</th>
+            <th width="40%" class="text-center">Nombre</th>
+            <th width="15%" class="text-center">Cantidad</th>
             <th width="20%" class="text-center">Precio</th>
             <th width="20%" class="text-center">Total</th>
             <th width="5%">--</th>
         </tr>
         <?php $total=0; ?>
-        <?php foreach($_SESSION['CARRITO2'] as $indice=>$producto){ ?>
+        <?php foreach($_SESSION['CARRITO2'] as $indice => $producto){ ?>
         <tr>
-            <td width="40%"> <?php echo $producto['NOMBRE'] ?></td>
+            <td width="40%" class="text-center"><?php echo $producto['NOMBRE'] ?></td>
             <td width="15%" class="text-center"><?php echo $producto['CANTIDAD'] ?></td>
             <td width="20%" class="text-center"><?php echo $producto['PRECIO'] ?></td>
-            <td width="20%" class="text-center"><?php echo number_format($producto['PRECIO']*$producto['CANTIDAD'],2);  ?></td>
+            <td width="20%" class="text-center"><?php echo number_format($producto['PRECIO'] * $producto['CANTIDAD'], 2); ?></td>
             <td width="5%">
-                
-            <form action="" method="post">
-                
-                <input type="hidden" 
-                name="id" 
-                id="id" 
-                value="<?php echo  openssl_encrypt ($producto['ID'],COD,KEY);?>" >
-
-                <button class="btn btn-danger"
-                type="submit"
-                name="btnAccion"
-                value="Eliminar"
-                >Elminar</button>
-
-            </form>
-
-
-
-           </td>
+                <form action="" method="post">
+                    <input type="hidden" name="id" id="id" value="<?php echo openssl_encrypt($producto['ID'], COD, KEY); ?>">
+                    <input type="number" name="cantidadEliminar" id="cantidadEliminar" value="1" min="1" max="<?php echo $producto['CANTIDAD']; ?>">
+                    <button class="btn btn-danger" type="submit" name="btnAccion" value="Eliminar">Eliminar</button>
+                </form>
+            </td>
         </tr>
-        <?php $total=$total+($producto['PRECIO']*$producto['CANTIDAD']); ?>
+        <?php $total += ($producto['PRECIO'] * $producto['CANTIDAD']); ?>
         <?php } ?>
         <tr>
             <td colspan="3" align="right"><h3>Total</h3></td>
-            <td align="right"><h3><?php echo number_format($total,2);?></h3></td>
+            <td align="right"><h3><?php echo number_format($total, 2); ?></h3></td>
             <td></td>
         </tr>
     </tbody>
 </table>
 <?php } else { ?>
-
     <div class="alert alert-success" role="alert">
         No hay productos en el carrito...
     </div>
+<?php } ?>
 
-    <?php }?>
+<?php
+// Código para manejar las acciones del carrito
+if(isset($_POST['btnAccion'])) {
+
+    switch($_POST['btnAccion']) {
+        case 'Agregar':
+            // Código para agregar productos (ya existente)
+            break;
+
+        case 'Eliminar':
+            if (is_numeric(openssl_decrypt($_POST['id'], COD, KEY))) {
+                $ID = openssl_decrypt($_POST['id'], COD, KEY);
+
+                foreach($_SESSION['CARRITO2'] as $indice => $producto) {
+                    if ($producto['ID'] == $ID) {
+                        // Obtener la cantidad a eliminar
+                        $cantidadEliminar = isset($_POST['cantidadEliminar']) ? intval($_POST['cantidadEliminar']) : 1;
+
+                        if ($cantidadEliminar < 0) {
+                            // Reducir la cantidad del producto
+                            $_SESSION['CARRITO2'][$indice]['CANTIDAD'] -= $cantidadEliminar;
+
+                            // Eliminar el producto si la cantidad es 0 o menor
+                            if ($_SESSION['CARRITO2'][$indice]['CANTIDAD'] <= 0) {
+                                unset($_SESSION['CARRITO2'][$indice]);
+                            }
+
+                            // Reindexar el array para evitar problemas de índices no consecutivos
+                            $_SESSION['CARRITO2'] = array_values($_SESSION['CARRITO2']);
+
+                            echo "<script>alert('Cantidad actualizada en el carrito.');</script>";
+                        } else {
+                            echo "<script>alert('Cantidad a eliminar no válida.');</script>";
+                        }
+
+                        break;
+                    }
+                }
+            } else {
+                $mensaje = 'ups ID incorrecto' . "<br/>";
+            }
+            break;
+    }
+}
+?>
